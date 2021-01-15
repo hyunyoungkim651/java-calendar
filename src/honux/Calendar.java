bedbec80f1081ae731986c7feb433b604d84eb2b
hyunyoungkim651 <k00810111@gmail.com>
2021-01-15 12:38:32 +0900

    remove if-else change switch

--- src/honux/Calendar.java
+++ src/honux/Calendar.java
@@ -1,27 +1,57 @@
 package honux;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 public class Calendar {
     private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    private static final String[] WEEKDAY = {"SU", "MO", "WE", "TH", "FR", "SA"};
+    private static final int[] LEEP_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final String[] WEEKDAY = {"SU", "MO", "Tu", "WE", "TH", "FR", "SA"};
+
+    private HashMap<Date, String> planMap;
+
+    public Calendar() {
+        planMap = new HashMap<Date, String>();
+    }
+
+
+    /**
+     *
+     * @param strdate ex : "2017-06-20"
+     * @param plan
+     */
+    public void registerPlan(String strdate, String plan) throws ParseException {
+        Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(strdate);
+        planMap.put(date1, plan);
+    }
+
+    public String searchPlan(String strDate) throws ParseException {
+        Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+        String plan = planMap.get(date1);
+        return plan;
+    }
 
     private int getWeekDay(int year, int month, int day) {
         // 기준 년도
         int syear = 1970;
-        final int STANDARD_WEEKDAY = 3; // 1970/1/1 = Thursday
+        final int STANDARD_WEEKDAY = 4; // 1970/1/1 = Thursday
 
         int count = 0;
+
         for (int i = syear; i < year; i++) {
-            int delta = (MAX_DAYS[2] == 29) ? 366 : 365;
+            int delta = isLeepYear(i) ? 366 : 365;
             count += delta;
         }
 
         for (int i = 1; i < month; i++) {
-            int delta = MAX_DAYS[month];
+            int delta = getMaxDaysOfMonth(year, i);
             count += delta;
         }
 
+        // 1월 1일은 더해줄 필요 없다.
         count += day - 1;
 
         int weekday = (count + STANDARD_WEEKDAY) % 7;
@@ -29,9 +59,20 @@ public class Calendar {
         return weekday;
     }
 
-    public void isLeepYear(int year) {
+    public boolean isLeepYear(int year) {
         if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-            MAX_DAYS[2] = 29;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getMaxDaysOfMonth(int year, int month) {
+        if (isLeepYear(year)) {
+            return LEEP_MAX_DAYS[month];
+        }
+        else{
+            return MAX_DAYS[month];
         }
     }
 
@@ -41,7 +82,7 @@ public class Calendar {
         System.out.println(" SU MO TU WE TH FR SA");
         System.out.println("----------------------");
 
-        isLeepYear(year);
+
         // get weekday automatically
         int weekday = getWeekDay(year, month, 1);
 
@@ -58,7 +99,7 @@ public class Calendar {
             System.out.print(String.format("%3d", i));
         }
 
-        System.out.println("");
+        System.out.println();
 
         // print second line
         count++;
@@ -74,5 +115,11 @@ public class Calendar {
 //        System.out.println("8   9   10  11  12  13  14");
 //        System.out.println("15  16  17  18  19  20  21");
 //        System.out.println("22  23  24  25  26  27  28");
+    }
+
+    public static void main(String[] args) throws ParseException {
+        Calendar cal = new Calendar();
+        cal.registerPlan("2017-06-23", "Let's eat beef!");
+        System.out.println(cal.searchPlan("2017-06-23").equals("Let's eat beef!"));
     }
 }
--- src/honux/Prompt.java
+++ src/honux/Prompt.java
@@ -1,10 +1,24 @@
 package honux;
 
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Prompt {
 
-    private final static String PROMPT = "cal> ";
+    public void printMenu() {
+        System.out.println("+----------------------+");
+        System.out.println("| 1. 일정 등록");
+        System.out.println("| 2. 일정 검색");
+        System.out.println("| 3. 달력 보기");
+        System.out.println("| h. 도움말 q. 종료");
+        System.out.println("+----------------------+");
+    }
+
+    /**
+     * 1. switch case - String 적용
+     * 2. Plan class - refactoring
+     *
+     */
 
     /*
         @param week 요일명
@@ -18,62 +32,102 @@ public class Prompt {
 //        else if (week.equals("th")) return 4;
 //        else if (week.equals("fr")) return 5;
 //        else if (week.equals("sa")) return 6;
-//        else return 0;
-        int week_num = 0;
+//        else return;
 
-        switch (week){
-            case "su":
-                week_num = 0;
-                break;
+        switch (week) {
             case "mo":
-                week_num = 1;
-                break;
+                return 1;
             case "tu":
-                week_num = 2;
-                break;
+                return 2;
             case "we":
-                week_num = 3;
-                break;
+                return 3;
             case "th":
-                week_num = 4;
-                break;
+                return 4;
             case "fr":
-                week_num = 5;
-                break;
+                return 5;
             case "sa":
-                week_num = 6;
-                break;
+                return 6;
             default:
-                System.out.println("잘못 입력 ");
+                return 0;
         }
-        return week_num;
     }
 
-    public void runPrompt() {
+    public void runPrompt() throws ParseException {
+        printMenu();
         Scanner sc = new Scanner(System.in);
         Calendar cal = new Calendar();
 
-        while (true) {
-            System.out.println("년도를 입력하시오.(exit : -1)");
-            System.out.print("YEAR> ");
-            int year = sc.nextInt();
-            if (year == -1) {
-                break;
-            }
-            System.out.println("달을 입력하시오.");
-            System.out.print("MONTH> ");
-            int month = sc.nextInt();
-            if (month > 12 || month < 1) {
-                System.out.println("잘못된 입력입니다.");
-                continue;
-            }
-            cal.printCalendar(year, month);
-        }
 
-        System.out.println("Bye~");
+        boolean isLoop = true;
+        while (isLoop) {
+            System.out.println("명령 (1, 2, 3, h, q)");
+            String cmd = sc.next();
+            switch (cmd) {
+                case "1":
+                    cmdRegister(sc, cal);
+                    break;
+                case "2":
+                    cmdSearch(sc, cal);
+                    break;
+                case "3":
+                    cmdCal(sc, cal);
+                    break;
+                case "h":
+                    printMenu();
+                    break;
+                case "q":
+                    isLoop = false;
+                    break;
+            }
+        }
+        System.out.println("Thank you, Bye~");
     }
 
-    public static void main(String[] args) {
+    private void cmdCal (Scanner s, Calendar c){
+        System.out.println("년도를 입력하시오.(exit : -1)");
+        System.out.print("YEAR> ");
+        int year = s.nextInt();
+
+        System.out.println("달을 입력하시오.");
+        System.out.print("MONTH> ");
+        int month = s.nextInt();
+
+        if (month > 12 || month < 1) {
+            System.out.println("잘못된 입력입니다.");
+            return;
+        }
+
+        c.printCalendar(year, month);
+    }
+
+
+    private void cmdSearch(Scanner s, Calendar c) {
+        System.out.println("[일정 검색]");
+        System.out.println("[날짜를 입력해 주세요(yyyy-MM--dd).]");
+        String date = s.next();
+        String plan = "";
+        try {
+            plan = c.searchPlan(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.out.println("일정 검색 중 오류가 발생했습니다.");
+        }
+        System.out.println(plan);
+    }
+
+    private void cmdRegister(Scanner s,  Calendar c) throws ParseException {
+        System.out.println("[새 일정 등록]");
+        System.out.println("[날짜를 입력해 주세요(yyyy-MM--dd).]");
+        String date = s.next();
+        String text = "";
+        s.nextLine();
+        System.out.println("일정을 입력해 주세요.");
+        text = s.nextLine();
+
+        c.registerPlan(date, text);
+    }
+
+    public static void main(String[] args) throws ParseException {
         // 셀 실행
         Prompt p = new Prompt();
         p.runPrompt();
